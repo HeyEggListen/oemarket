2687cabb251a20bac24dbd9c1c0a2d9406e381cf
kosmo <kosmo@DESKTOP-EL5IHTJ>
2024-06-03 18:32:57 +0900

    commit

---
+++ oe-jk/oeMarket/src/oe/market/board/controller/OeBoardController.java
@@ -0,0 +1,13 @@
+package oe.market.board.controller;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OeBoardController {
+	Logger logger = LogManager.getLogger(OeBoardController.class);
+	
+	@Autowired(required=false)
+	private Kos
+}
